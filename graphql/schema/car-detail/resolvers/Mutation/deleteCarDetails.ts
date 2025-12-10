5b9617d6b6051ee85a25c7f8f6d0ba050654d9ef
Shea Riley <sheariley@live.com>
2025-12-10 00:25:29 -0600

    WIP: Fix deleteCarDetails mutation error rel FK to CarDetailFeature

--- graphql/schema/car-detail/resolvers/Mutation/deleteCarDetails.ts
+++ graphql/schema/car-detail/resolvers/Mutation/deleteCarDetails.ts
@@ -1,9 +1,14 @@
 import type { MutationResolvers } from '@/graphql/generated/types.generated'
 
 export const deleteCarDetails: NonNullable<MutationResolvers['deleteCarDetails']> = async (_parent, _arg, _ctx) => {
-  const result = await _ctx.dbClient.carDetail.deleteMany({
-    where: { id: { in: _arg.ids } },
-  })
+  const result = await _ctx.dbClient.$transaction([
+    _ctx.dbClient.carDetailFeature.deleteMany({
+      where: { carDetailId: { in: _arg.ids } }
+    }),
+    _ctx.dbClient.carDetail.deleteMany({
+      where: { id: { in: _arg.ids } }
+    })
+  ])
 
-  return result.count
+  return result[1].count
 }
