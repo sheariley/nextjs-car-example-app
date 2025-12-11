ae321d60cd80cd42ace5bea73624dec5deca1144
Shea Riley <sheariley@live.com>
2025-12-11 16:19:22 -0600

    Fix lint warnings

--- graphql/schema/car-detail/resolvers/CarDetailPage.ts
+++ graphql/schema/car-detail/resolvers/CarDetailPage.ts
@@ -1,4 +1,4 @@
-import type   { CarDetailPageResolvers } from './../../../generated/types.generated';
+import type   { CarDetailPageResolvers } from './../../../generated/types.generated'
     export const CarDetailPage: CarDetailPageResolvers = {
     /* Implement CarDetailPage resolver logic here */
-  };
+  }
--- lib/coerceErrorMessage.ts
+++ lib/coerceErrorMessage.ts
@@ -1,4 +1,4 @@
 export default function coerceErrorMessage(error: unknown, unknownMessage: string = 'Unknown error') {
-  if (error instanceof Error) return error.message;
-  return unknownMessage;
+  if (error instanceof Error) return error.message
+  return unknownMessage
 }
