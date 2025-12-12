340dc0f071c67ccec6c972b909cfab6dc90aebb9
Shea Riley <sheariley@live.com>
2025-12-11 21:32:42 -0600

    Fix handler type mismatch (final...lol)

--- app/api/graphql/route.ts
+++ app/api/graphql/route.ts
@@ -1,5 +1,12 @@
 import { createGraphQLServerHandler } from '@/graphql/server/server-handler'
+import { NextRequest } from 'next/server'
 
 const handler = createGraphQLServerHandler()
 
-export { handler as GET, handler as POST }
+export async function GET(request: NextRequest) {
+  return handler(request)
+}
+
+export async function POST(request: NextRequest) {
+  return handler(request)
+}
